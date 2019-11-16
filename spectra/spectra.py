9da7abe2c4098a0af001aa978a8cc07962089658
ruimpz <ruimpz@gmail.com>
2019-11-16 23:36:42 +0000

    database comparison

--- spectra/spectra.py
+++ spectra/spectra.py
@@ -1,6 +1,7 @@
 from scipy import signal as sg
 from scipy import optimize as opt
 from astropy.io import fits
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -39,7 +40,7 @@ def limit_spec(data, lambda_min, lambda_max):
     data1, data2 = data
     wl = data1[(data1>lambda_min)&(data1<lambda_max)]
     flux = data2[(data1>lambda_min)&(data1<lambda_max)]
-    return wl, flux
+    return np.array([wl, flux])
 
 def gauss(x, a, b, c,d):
     """Returns numpy array
@@ -62,9 +63,9 @@ def ajust_gauss(data):
 
     Takes spectra data, minimum and maximum wavelengths and returns parameters of gaussian fit
     """
-    params0=[-1, data[0][data[1].argmin()], .1, 1]
+    params0=[-1, data[0][data[1].argmin()], .1, data[0][data[1].argmax()]]
     popt, r = (0, 0, 0, 0), 0
-    while r < .8 and params0[2] > 0:
+    while r < .9 and params0[2] > 0:
         try:
             popt, pcov = opt.curve_fit(gauss, *data, params0)
             y = gauss(data[0], *popt)
@@ -78,33 +79,53 @@ def ajust_gauss(data):
 
 
 
-def get_W(data):
+def get_W(data, r_tol = .994):
     """Returns float
 
     Takes spectra data, minimum and maximum wavelengths of line limit and return equivalent width W_lambda.
     """
     ps, r = ajust_gauss(data)
-    return W_gauss(*ps)
+    if r > r_tol:
+        return W_gauss(*ps), ps, r
+    else:
+        return 0, 0, 0
+
+def get_line_Ws(data, ws, k = .2, get_inds = False, get_zeros=False):
+    W = np.zeros(len(ws))
+    for i, wl in enumerate(ws):
+        line = limit_spec(data, wl-k, wl+k)
+        W[i], p, r = get_W(line)
+#        if r > .98:
+#            print(r)
+#           plt.plot(line[0], line[1])
+#            plt.plot(line[0], gauss(line[0], *p))
+#            plt.show()
+    inds = np.where(W != 0)
+    if get_zeros:
+        return W
+    elif get_inds:
+        return ws[inds], W[inds], inds
+    else:
+        return ws[inds], W[inds]
 
 
-
-def get_temp_estimate(data, EP1, EP2, wls1, wls2, lgf1, lgf2, k=.4, N = 1000):
+def get_temp_estimate(data, EP1, EP2, wls1, wls2, lgf1, lgf2):
     """Returns float
 
     Takes observed spectra data, EP ranges, corresponding wavelength and log(gf) lists and estimates temperature.
     Takes optional parameters k and N for line width estimation and number of points in linear regression, respectively.
     """
-    W1, W2 = np.empty(wls1.size), np.empty(wls2.size)
-    for i, wl in enumerate(wls1):
-        line = limit_spec(data, wl - k, wl + k)
-        W1[i] = get_W(line)
-    for i, wl in enumerate(wls2):
-        line = limit_spec(data, wl - k, wl + k)
-        W2[i] = get_W(line)
+    wls1, W1, inds1 = get_line_Ws(data, wls1, get_inds=True)
+    wls2, W2, inds2 = get_line_Ws(data, wls2, get_inds=True)
+    lgf1 = lgf1[inds1]
+    lgf2 = lgf2[inds2]
     x1 = np.array(lgf1 + np.log10(wls1))
-    y1 = np.absolute(np.array(W1*1000/wls1))
+    y1 = np.array(W1*1000/wls1)
     x2 = np.array(lgf2 + np.log10(wls2))
-    y2 = np.absolute(np.array(W2*1000/wls2))
+    y2 = np.array(W2*1000/wls2)
+#    plt.plot(x1, y1, "+")
+#    plt.plot(x2, y2, "+")
+#    plt.show()
     p1, o1 = opt.curve_fit(lambda x, a, b: a*x+b, x1, y1)
     p2, o2 = opt.curve_fit(lambda x, a, b: a*x+b, x2, y2)
     ymin, ymax = max(y1[0], y2[0]), min(y1[-1], y2[-1])
@@ -125,8 +146,6 @@ def get_temp_range(T, delta_T=400):
 
 def read_library(library_path, Ts, logg_min = 3.5):
     """Returns list
-
-    Takes library path and returns possible spectra to analyze in a temperature range.
     """
     data = []
     for file in os.listdir(library_path):
@@ -135,4 +154,18 @@ def read_library(library_path, Ts, logg_min = 3.5):
                 data.append(file)
     return data
 
+def get_spec_fit(library, wl_obs, W_obs, \
+                 library_path="../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/", \
+                 lambda_file="../imagens_teste/GES_UVESRed580_Lambda.fits"):
+    """Returns string.
+
+    Compares equivalent width of lines of observed spectra with database of synthetic spectra and returns name of best fitting synthetic spectra.
+    """
+    diffs = np.zeros(len(library))
+    for i, synt_spec in enumerate(library):
+        synt_data = np.array(read_sintfile(lambda_file, library_path + synt_spec))
+        W_synt = get_line_Ws(synt_data, wl_obs, get_zeros=True)
+        diff = (W_obs*1000 - W_synt*1000)**2
+        diffs[i] = diff.sum()
+    return library[np.argmin(diffs)]
 
--- spectra/temp_estimate_test.py
+++ spectra/temp_estimate_test.py
@@ -4,39 +4,72 @@ import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import spectra as spt
 
-data = np.array(spt.read_obsfile("../estrelas_a_analisar/estrela2_vcor.fits"))
-#data = np.array(spt.read_obsfile("../estrelas_a_analisar/estrela1.fits"))
-
+#data0 = np.array(spt.read_obsfile("../estrelas_a_analisar/estrela2_vcor.fits"))
+data0 = np.array(spt.read_obsfile("../estrelas_a_analisar/estrela1.fits"))
+data1 = np.array(spt.read_sintfile("../imagens_teste/GES_UVESRed580_Lambda.fits", "../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/p4250:g+4.0:m0.0:t01:z-1.00:a+0.40.GES4750.fits"))
+#data1 = np.array(spt.read_sintfile("../imagens_teste/GES_UVESRed580_Lambda.fits", "../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/p5750:g+3.5:m0.0:t01:z-1.00:a+0.40.GES4750.fits"))
 line_df = pd.read_csv("~/University/Comp_Astro/Project_1/line_list_tsantaki.dat", sep="\t")
 df = line_df[line_df.El.str.contains("FeI ")]
-
+wls, EP, lgf, EW = np.array(df["lambda"]), np.array(df["EP"]), np.array(df["loggf"]), np.array(df["EW"])
 EP1 = [2.1, 2.3]
 EP2 = [4.5, 4.6]
-wls, EP, lgf, EW = df["lambda"], df["EP"], df["loggf"], df["EW"]
 
-wl1 = wls[(EP > EP1[0]) & (EP < EP1[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
-wl2 = wls[(EP > EP2[0]) & (EP < EP2[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
-lgf1 = lgf[(EP > EP1[0]) & (EP < EP1[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
-lgf2 = lgf[(EP > EP2[0]) & (EP < EP2[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
-EW1 = EW[(EP > EP1[0]) & (EP < EP1[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
-EW2 = EW[(EP > EP2[0]) & (EP < EP2[1]) & (wls > data[0,0]) & (wls < data[0, -1])]
+#limit spectra to usable part
+data0 = spt.limit_spec(data0, 5900, 6500)
+#plt.plot(data1[0], data1[1])
+#plt.show()
 
-
-k = .2
-wls = wls[(wls>data[0][0]) & (wls<data[0][-1])]
-W = np.zeros(wls.size)
-for i, wl in enumerate(wls):
-    line = spt.limit_spec(data, wl - k, wl + k)
-    ps, r = spt.ajust_gauss(line)
-    if r > .8:
-        W[i] = spt.get_W(line)
-
-good_wls = wls[W > 0.]
-good_W = W[W != 0.]
+#get line data and limit to obs spectra
+valid_lines1 = np.where((EP > EP1[0]) & (EP < EP1[1]) & (wls > data0[0,0]) & (wls < data0[0, -1]))
+valid_lines2 = np.where((EP > EP2[0]) & (EP < EP2[1]) & (wls > data0[0,0]) & (wls < data0[0, -1]))
+wl1 = wls[valid_lines1]
+wl2 = wls[valid_lines2]
+lgf1 = lgf[valid_lines1]
+lgf2 = lgf[valid_lines2]
+EW1 = EW[valid_lines1]
+EW2 = EW[valid_lines2]
+wls = wls[(wls > data0[0,0]) & (wls < data0[0, -1])]
 
 
-#T = spt.get_temp_estimate(data, EP1, EP2, wl1, wl2, lgf1, lgf2)
-#print(T)
-#t_range = spt.get_temp_range(T)
-#print(t_range)
+T0 = spt.get_temp_estimate(data0, EP1, EP2, wl1, wl2, lgf1, lgf2)
+t_range = spt.get_temp_range(T0)
+
+lib = spt.read_library("../library/GES_UVESRed580_deltaAlphaFe+0.0_fits", t_range)
+
+w0, W0 = spt.get_line_Ws(data0, wls)
+
+data1 = np.array(spt.read_sintfile("../imagens_teste/GES_UVESRed580_Lambda.fits", "../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/{}".format(lib[0])))
+#plt.plot(data1[0], data1[1])
+#plt.show()
+
+#w1, W1, inds_compare = spt.get_line_Ws(data1, w0, k = .2, get_inds=True)
+
+
+#diffs = np.zeros(len(lib))
+#for i, fit in enumerate(lib):
+#    data1 = np.array(spt.read_sintfile("../imagens_teste/GES_UVESRed580_Lambda.fits", "../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/{}".format(fit)))
+#    W1 = spt.get_line_Ws(data1, w0, k = .15, get_zeros=True, get_inds=True)
+#    diff = (W0*1000 - W1*1000)**2
+#    diffs[i] = diff.sum()
+
+
+best_fit = spt.get_spec_fit(lib, w0, W0)
+print(T0)
+datab = np.array(spt.read_sintfile("../imagens_teste/GES_UVESRed580_Lambda.fits", "../library/GES_UVESRed580_deltaAlphaFe+0.0_fits/{}".format(best_fit)))
+plt.plot(datab[0], datab[1])
+plt.show()
+
+# get Ws from obs data and relevant lines
+#print(w0)
+#print(W0*1000)
+
+#print(W1*1000)
+#print(inds_compare)
+
+
+
+
+
+
+
 
