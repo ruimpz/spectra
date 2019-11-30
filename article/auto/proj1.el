82d2bac902e46846c705377c4ae0358cd44b1ae2
ruimpz <ruimpz@gmail.com>
2019-11-30 23:08:40 +0000

    readme

--- article/auto/proj1.el
+++ article/auto/proj1.el
@@ -1,6 +1,14 @@
 (TeX-add-style-hook
  "proj1"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "aa"
@@ -9,7 +17,8 @@
     "txfonts"
     "amsmath"
     "subfigure"
-    "booktabs")
+    "booktabs"
+    "hyperref")
    (LaTeX-add-labels
     "eq:W_estimation"
     "fig:line_fit"
